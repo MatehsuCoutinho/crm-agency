077352d299b11075ba4948d765d310d7023645bb
MatehsuCoutinho <matheuscoutinho.0420@gmail.com>
2026-02-23 21:25:48 -0300

    fix: fix prisma error

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,6 +1,6 @@
+import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import authRoutes from "./modules/auth/auth.routes";
 import { authMiddleware } from "./middlewares/auth.middleware";
 
